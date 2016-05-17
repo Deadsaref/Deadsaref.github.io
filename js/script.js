5147540af2a3e8eb217f633f1d303002defcd351
Deadsaref <ognemetchikk@yandex.ru>
2016-05-17 23:06:39 +0300

    Третья правка финальной версии
    Исправил ошибки

--- js/script.js
+++ js/script.js
@@ -84,46 +84,45 @@ if (LinkWriteUs) {
 //Сервис-слайдер
 
 $(document).ready(function () {
-  var menuElementPath = '#menu li';
-  var slidePath = '.slide';
+  var menuElementPath = "#menu li";
+  var slidePath = ".slide";
   $(menuElementPath).click(function () {
-    $(menuElementPath).removeClass('active');
-    $(this).addClass('active');
-    $(slidePath).removeClass('active');   
-    var c = $(this).attr('id').split('-');
-    $(slidePath+'[class*="'+c[1]+'"]').addClass('active');
+    $(menuElementPath).removeClass("active");
+    $(this).addClass("active");
+    $(slidePath).removeClass("active");   
+    var c = $(this).attr("id").split("-");
+    $(slidePath+"[class*="+c[1]+"]").addClass("active");
   });
 });
 
 //Слайдер на главной
 
 $(document).ready(function() {
-    $('.sliderBox > .controls > .point').bind('click', function() {
-        if ($(this).attr('class') == 'point active') {
+    $(".sliderBox > .controls > .point").bind("click", function() {
+        if ($(this).attr("class") == "point active") {
             return false;
         }
-        $('.sliderBox > .controls > .point').removeClass('active');
-        $(this).addClass('active');
-        $('.sliderBox > .images').css('left', '-' + $('.point').index(this) * 620 + 'px');
+        $(".sliderBox > .controls > .point").removeClass("active");
+        $(this).addClass("active");
+        $(".sliderBox > .images").css("left", "-" + $(".point").index(this) * 620 + "px");
     });
 
-    $('.sliderBox > .controls > .slide').bind('click', function() {
-        var imageID = $('[class="point active"]').index('.sliderBox > .controls > [class*="point"]');
-        var imagesCount = $('.sliderBox > .controls > .point').length - 1;
-
-        if ($(this).attr('id') == 'right') {
+    $(".sliderBox > .controls > .slide").bind("click", function() {
+        var imageID = $(".point.active").index(".point");
+        var imagesCount = $(".sliderBox > .controls > .point").length - 1;
+        if ($(this).attr("id") == "right") {
             if (imageID == imagesCount) {
-                $($('.sliderBox > .controls > .point'))[0].click();
+                $($(".sliderBox > .controls > .point"))[0].click();
             } else {
                 imageID++;
-                $($('.sliderBox > .controls > .point'))[imageID].click();
+                $($(".sliderBox > .controls > .point"))[imageID].click();
             }
-        } else if ($(this).attr('id') == 'left') {
+        } else if ($(this).attr("id") == "left") {
             if (imageID == 0) {
-                $($('.sliderBox > .controls > .point'))[imagesCount].click();
+                $($(".sliderBox > .controls > .point"))[imagesCount].click();
             } else {
                 imageID--;
-                $($('.sliderBox > .controls > .point'))[imageID].click();
+                $($(".sliderBox > .controls > .point"))[imageID].click();
             }
         }
     });
